a698d699143f74ee575ece03c166464c460e3d98
Brandol Vargas <brandol.fontecha@moviired.co>
2024-02-07 13:29:02 -0500

    Preload and resize image

--- src/app/aplication/pages/App.jsx
+++ src/app/aplication/pages/App.jsx
@@ -10,7 +10,7 @@ function App () {
     <div className='relative z-10 min-h-screen font-custom px-4 lg:px-14' id='wrap-content'>
       <div className='overflow-hidden'>
         <div className="contrast-50 fixed left-0 h-full w-[100%] max-w-none object-cover object-center bg-no-repeat cursor-none pointer-events-none 
-        bg-[url('/backgrounds/bg-4-000fae5e.png')] aspect-[1/1] -z-10" ></div>
+        bg-[url('/backgrounds/bg-4-000fae5e.avif')] aspect-[1/1] -z-10" ></div>
       </div>
       <Header />
 
